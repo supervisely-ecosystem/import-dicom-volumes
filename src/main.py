8fa6489e8e33c92fe7c8851faf89c4a977452fda
Daniel Y <33942379+qanelph@users.noreply.github.com>
2022-06-21 20:21:14 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -54,7 +54,7 @@ def import_images_groups(
         )
     
     if g.REMOVE_SOURCE:
-        api.file.remove(team_id=g.TEAM_ID, path=g.INPUT_PATH)
+        api.file.remove(team_id=g.TEAM_ID, path=g.INPUT_DIR)
         source_dir_name = g.INPUT_PATH.lstrip("/").rstrip("/")
         sly.logger.info(
             msg=f"Source directory: '{source_dir_name}' was successfully removed."
