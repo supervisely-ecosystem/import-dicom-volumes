cf58f5b47c2579fff330b81fcedb11ec0007b149
peixed351 <120389559+peixed351@users.noreply.github.com>
2023-02-15 15:35:23 +0300

    fix import from archive (#4)

--- src/main.py
+++ src/main.py
@@ -10,10 +10,12 @@ def import_images_groups(
     api: sly.Api, task_id: int, context: dict, state: dict, app_logger
 ) -> None:
 
-    project_dir = f.download_data_from_team_files(
-        api=api, task_id=task_id, save_path=g.STORAGE_DIR
+    project_dir = f.download_data_from_team_files(api=api, task_id=task_id, save_path=g.STORAGE_DIR)
+    project_name = (
+        f.get_project_name_from_input_path(project_dir)
+        if len(g.OUTPUT_PROJECT_NAME) == 0
+        else g.OUTPUT_PROJECT_NAME
     )
-    project_name = f.get_project_name_from_input_path(project_dir) if len(g.OUTPUT_PROJECT_NAME) == 0 else g.OUTPUT_PROJECT_NAME
 
     project = g.api.project.create(
         workspace_id=g.WORKSPACE_ID,
@@ -30,9 +32,7 @@ def import_images_groups(
     for serie_id, files in series_infos.items():
         item_path = files[0]
         if sly.volume.get_extension(path=item_path) is None:
-            sly.logger.warn(
-                f"Can not recognize file extension {item_path}, serie will be skipped"
-            )
+            sly.logger.warn(f"Can not recognize file extension {item_path}, serie will be skipped")
             continue
         name = f"{sly.fs.get_file_name(path=item_path)}.nrrd"
         g.api.volume.upload_dicom_serie_paths(
@@ -50,14 +50,16 @@ def import_images_groups(
         g.api.volume.upload_nrrd_serie_path(
             dataset_id=dataset.id, name=name, path=nrrd_path, log_progress=True
         )
-    
+
     if g.REMOVE_SOURCE and not g.IS_ON_AGENT:
-        api.file.remove(team_id=g.TEAM_ID, path=g.INPUT_DIR)
-        source_dir_name = g.INPUT_DIR.lstrip("/").rstrip("/")
-        sly.logger.info(
-            msg=f"Source directory: '{source_dir_name}' was successfully removed."
-        )
-    
+        if g.INPUT_DIR is not None:
+            path_to_remove = g.INPUT_DIR
+        else:
+            path_to_remove = g.INPUT_FILE
+        api.file.remove(team_id=g.TEAM_ID, path=path_to_remove)
+        source_dir_name = path_to_remove.lstrip("/").rstrip("/")
+        sly.logger.info(msg=f"Source directory: '{source_dir_name}' was successfully removed.")
+
     g.my_app.stop()
 
 
