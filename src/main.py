988a78f3b1f81f2ef91a3bfed899f8e2ed569928
Almaz <79905215+almazgimaev@users.noreply.github.com>
2023-10-16 15:17:16 +0200

    Check if the project_dir is empty (#11)
    * add warning if no volumes were uploaded
    * do not create empty project

--- src/main.py
+++ src/main.py
@@ -9,7 +9,6 @@ import sly_globals as g
 def import_dicom_volumes(
     api: sly.Api, task_id: int, context: dict, state: dict, app_logger
 ) -> None:
-
     project_dir = f.download_data_from_team_files(api=api, task_id=task_id, save_path=g.STORAGE_DIR)
     project_name = (
         f.get_project_name_from_input_path(project_dir)
@@ -17,49 +16,57 @@ def import_dicom_volumes(
         else g.OUTPUT_PROJECT_NAME
     )
 
-    project = g.api.project.create(
-        workspace_id=g.WORKSPACE_ID,
-        name=project_name,
-        type=sly.ProjectType.VOLUMES,
-        change_name_if_conflict=True,
-    )
-    dataset = g.api.dataset.create(
-        project_id=project.id, name=g.DEFAULT_DATASET_NAME, change_name_if_conflict=True
-    )
-
-    used_volumes_names = []
-
     # DICOM
     series_infos = sly.volume.inspect_dicom_series(root_dir=project_dir)
-    for serie_id, files in series_infos.items():
-        item_path = files[0]
-        if sly.volume.get_extension(path=item_path) is None:
-            sly.logger.warn(f"Can not recognize file extension {item_path}, serie will be skipped")
-            continue
-        name = f"{serie_id}.nrrd"
-        name = f.generate_free_name(
-            used_names=used_volumes_names, possible_name=name, with_ext=True
-        )
-        used_volumes_names.append(name)
-        g.api.volume.upload_dicom_serie_paths(
-            dataset_id=dataset.id,
-            name=name,
-            paths=files,
-            log_progress=True,
-            anonymize=g.ANONYMIZE_VOLUMES,
-        )
-
     # NRRD
     nrrd_paths = sly.volume.inspect_nrrd_series(root_dir=project_dir)
-    for nrrd_path in nrrd_paths:
-        name = sly.fs.get_file_name_with_ext(path=nrrd_path)
-        name = f.generate_free_name(
-            used_names=used_volumes_names, possible_name=name, with_ext=True
+
+    if len(series_infos) == 0 and len(nrrd_paths) == 0:
+        sly.logger.warn("No volumes were uploaded. Please check the input directory.")
+    else:
+        project = g.api.project.create(
+            workspace_id=g.WORKSPACE_ID,
+            name=project_name,
+            type=sly.ProjectType.VOLUMES,
+            change_name_if_conflict=True,
         )
-        used_volumes_names.append(name)
-        g.api.volume.upload_nrrd_serie_path(
-            dataset_id=dataset.id, name=name, path=nrrd_path, log_progress=True
+        dataset = g.api.dataset.create(
+            project_id=project.id, name=g.DEFAULT_DATASET_NAME, change_name_if_conflict=True
         )
+
+        used_volumes_names = []
+
+        # DICOM
+        for serie_id, files in series_infos.items():
+            item_path = files[0]
+            if sly.volume.get_extension(path=item_path) is None:
+                sly.logger.warn(
+                    f"Can not recognize file extension {item_path}, serie will be skipped"
+                )
+                continue
+            name = f"{serie_id}.nrrd"
+            name = f.generate_free_name(
+                used_names=used_volumes_names, possible_name=name, with_ext=True
+            )
+            used_volumes_names.append(name)
+            g.api.volume.upload_dicom_serie_paths(
+                dataset_id=dataset.id,
+                name=name,
+                paths=files,
+                log_progress=True,
+                anonymize=g.ANONYMIZE_VOLUMES,
+            )
+
+        # NRRD
+        for nrrd_path in nrrd_paths:
+            name = sly.fs.get_file_name_with_ext(path=nrrd_path)
+            name = f.generate_free_name(
+                used_names=used_volumes_names, possible_name=name, with_ext=True
+            )
+            used_volumes_names.append(name)
+            g.api.volume.upload_nrrd_serie_path(
+                dataset_id=dataset.id, name=name, path=nrrd_path, log_progress=True
+            )
 
     if g.REMOVE_SOURCE and not g.IS_ON_AGENT:
         if g.INPUT_DIR is not None:
